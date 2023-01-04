6c7bc8c33cecfee0531faaddde947f7706affcc5
shyaamex <shyaam.ex@gmail.com>
2023-01-04 11:30:44 +0530

    7 files added with initial commit

---
+++ 6_For_Loop.py
@@ -0,0 +1,17 @@
+for item in 'Hello world':
+    print(item)
+
+for item in ["Hello", "Python", "world"]:
+    print(item)
+
+for item in [1,2,4,5]:
+    print(item)
+
+for item in range(0, 10):
+    print(item)
+
+for item in range(0, 10, 2): #here it will srart from 0 to 10 ans step 2
+    print(item)
+
+
+
