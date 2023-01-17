a60ac527ff7ae9c3705ea336e2d51c369c4c8671
shyaamex <shyaam.ex@gmail.com>
2023-01-17 18:43:36 +0530

    Python file for comments was added with this commit

---
+++ 00 Comments.py
@@ -0,0 +1,16 @@
+# Comments in Python
+
+# Lines writter after a hash(#) in python are comments, as they are ignored by interpreter
+# Comments are used to write information about the code
+# Comments makes the code more readable
+
+""" Triple quotes are used to write
+    multi line comments in python
+    programming
+"""
+# The print statement
+print("Shyam")
+
+# Always Why and Hows not whats
+def sqaure(num):
+    return num*num
