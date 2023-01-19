18457b58068c286c1e61a1c2c971fc34d39522bd
shyaamex <shyaam.ex@gmail.com>
2023-01-19 23:51:48 +0530

    File for Python Classes is added

---
+++ 13 Python Classes.py
@@ -0,0 +1,20 @@
+# Python is an object oriented programming language.
+# Almost everything in Python is an object, with its properties and methods.
+
+
+#Creating a class 
+class Myclass:
+    def hello(self):
+        
+        print("Hello, This is Shyam")
+
+    
+
+
+# Creating an object of above class (Myclass)
+a = Myclass()
+
+
+# Calling a method of the class through its object
+a.hello()
+
