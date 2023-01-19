0850d8590469dba957bc28b3c1a126a7904c5c82
shyaamex <shyaam.ex@gmail.com>
2023-01-19 23:59:41 +0530

    File of Python classes was modified

--- 13 Python Classes.py
+++ 13 Python Classes.py
@@ -18,3 +18,13 @@ a = Myclass()
 # Calling a method of the class through its object
 a.hello()
 
+
+
+# You can modify properties of class with objects like this:
+a.var=30
+print(a.var)
+
+
+
+# You can delete the properties of objects as well
+del a.var
