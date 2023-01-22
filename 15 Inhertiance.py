a5b5b82aacf6ea62f568459253b7ae6c448a71e0
shyaamex <shyaam.ex@gmail.com>
2023-01-22 22:45:59 +0530

    Some comments added in inheritance file

--- 15 Inhertiance.py
+++ 15 Inhertiance.py
@@ -1,22 +1,31 @@
 # Inheritance in python
 
+
+# This is an empty class
 class first:
     pass #Pass means nothing
 
 
 
-
+# base class
 class Animal:  
     def speak(self):  
         print("Animal Speaking")  
+        
+        
 #The child class Dog inherits the base class Animal  
 class Dog(Animal):  
     def bark(self):  
         print("dog barking")  
+        
+        
 #The child class Dogchild inherits another child class Dog  
 class DogChild(Dog):  
     def eat(self):  
         print("Eating bread...")  
+        
+        
+        
 d = DogChild()  
 d.bark()  
 d.speak()  
